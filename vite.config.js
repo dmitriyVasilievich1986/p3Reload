888e2848a6b6623b40d5b6c8c605e77a9f5a3943
dmitriy <dmitriyvasil@gmail.com>
2024-06-07 21:59:33 +0200

    update config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "p3Reload",
+  // base: "p3Reload",
+  server: {
+    port: 3000,
+  },
+  preview: {
+    port: 3000,
+  },
+  resolve: {
+    alias: {
+      Constants: path.resolve(__dirname, "src/constants"),
+    },
+  },
 });
