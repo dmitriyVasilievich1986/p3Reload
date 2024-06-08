4b147d644e1843801fb2ae03c56dea179d489fbc
dmitriy <dmitriyvasil@gmail.com>
2024-06-08 13:20:08 +0200

    add days names

--- src/constants/monthsNames.js
+++ src/constants/monthsNames.js
@@ -12,3 +12,15 @@ export const monthNames = [
   "November",
   "December",
 ];
+
+export const daysNames = {
+  sunday: 0,
+  monday: 1,
+  tuesday: 2,
+  wednesday: 3,
+  thursday: 4,
+  friday: 5,
+  saturday: 6,
+};
+
+export const daysNamesIndex = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
