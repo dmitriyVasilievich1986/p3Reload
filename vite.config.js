ba0aeaba27871c3c6dff8ba1474d70d3d0cc4076
dmitriy <dmitriyvasil@gmail.com>
2024-06-09 00:05:42 +0200

    return base

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: "p3Reload",
+  base: "p3Reload",
   server: {
     port: 3000,
   },
