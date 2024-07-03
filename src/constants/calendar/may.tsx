38f0f0394f36f32c2f3d10b1d9affff87ac46235
dmitriy <dmitriyvasil@gmail.com>
2024-07-03 09:25:19 +0200

    add prerequisite for a `hanged man`

--- src/constants/calendar/may.tsx
+++ src/constants/calendar/may.tsx
@@ -94,6 +94,17 @@ export const may: singleDay[] = [
           </Choices>
         ),
       },
+      [Times.AfterSchool]: {
+        ...events.Special,
+        label: () => (
+          <EventCard
+            prerequisite='Give her "Weird Takoyaki" & "Mad Bull"'
+            place="Naganaki Shrine"
+            name="Maiko Oohashi"
+            head="Hanged Man"
+          />
+        ),
+      },
       [Times.Day]: events.Emperor,
       [Times.Evening]: events.gameParadeAcademics,
     },
