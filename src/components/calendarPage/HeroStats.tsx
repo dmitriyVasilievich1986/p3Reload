76dd5f76cab904ec9826e00465cd6b45f4beeeee
dmitriy <dmitriyvasil@gmail.com>
2024-07-13 14:25:26 +0200

    move stats points to tooltip

--- src/components/calendarPage/HeroStats.tsx
+++ src/components/calendarPage/HeroStats.tsx
@@ -3,6 +3,8 @@ import { stats } from "../../constants/stats";
 import { HeroStatsProps } from "./types";
 import classnames from "classnames/bind";
 import * as style from "./style.scss";
+import { Tooltip } from "../tootlip";
+import { Badge } from "../badge";
 import Card from "../card/Card";
 
 const cx = classnames.bind(style);
@@ -18,19 +20,42 @@ function HeroStats(props: HeroStatsProps) {
       : `${previousName} -> ${currentName}`;
   };
 
-  const getPoints = (name: StatsNames) => {
-    if (props.stats[name] >= stats[name].maxPoints) return "max level";
+  const getBadge = (name: StatsNames) => {
+    const levelName = stats[name].getLevel(props.stats[name]).name;
+    const previousName = props.previousDay
+      ? stats[name].getLevel(props.previousDay.stats[name]).name
+      : stats[name].levels[0].name;
+    const color = previousName === levelName ? "blue" : "green";
+    const level = stats[name].levels.map((l) => l.name).indexOf(levelName) + 1;
+    return <Badge value={String(level)} color={color} size="small" />;
+  };
+
+  const StatsTooltip = ({ name }: { name: StatsNames }) => {
     const level = stats[name].getLevel(props.stats[name]);
-    return ` (${props.stats[name]}/${level.nextLevel} pts.)`;
+    return (
+      <div className={cx("stats-tooltip-wrapper")}>
+        <div>
+          <label>Points:</label>
+          {props.stats[name]} pts.
+        </div>
+        <div>
+          <label>Next level:</label>
+          {level.nextLevel} pts.
+        </div>
+      </div>
+    );
   };
 
   return (
     <Card head="Stats" color="primary">
       <div className={cx("stat")}>
         {(Object.keys(StatsNames) as Array<StatsNames>).map((stat) => (
-          <div key={stat}>
-            <label>{stat}</label>: {getLevel(stats[stat].name)}
-            {getPoints(stats[stat].name)}
+          <div key={stat} className={cx("stat-item")}>
+            <Tooltip tooltip={<StatsTooltip name={stat} />}>
+              {getBadge(stat)}
+            </Tooltip>
+            <label>{stat}:</label>
+            {getLevel(stats[stat].name)}
           </div>
         ))}
       </div>
