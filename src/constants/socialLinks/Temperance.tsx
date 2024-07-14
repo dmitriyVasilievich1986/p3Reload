67d2a9d3da3c7eeebe3c65f80d2388f33ec50dba
dmitriy <dmitriyvasil@gmail.com>
2024-07-14 21:18:08 +0200

    update `Temperance` link

--- src/constants/socialLinks/Temperance.tsx
+++ src/constants/socialLinks/Temperance.tsx
@@ -3,6 +3,7 @@ import { EventCard } from "../../components/eventCard";
 
 import {
   baseSocialLinkCalculation,
+  mainCharName,
   LinkLevel,
   choices,
   choice,
@@ -15,34 +16,34 @@ export const Temperance: SocialLinkType = {
     LinkLevel(),
     LinkLevel(0, [
       choices(
-        "It will be my first time going, Will you maybe, how do you say, show me the ropes?",
+        "It will be my first time going. Will you maybe, how do you say, show me the ropes?",
         [
           choice({ label: "Sure, let's go.", points: 5 }),
-          choice({ label: "You like sweets?" }),
-          choice({ label: "There's nothing to show.", points: 15 }),
+          choice({ label: "You like sweets?", points: 5 }),
+          choice({ label: "There's nothing to show.", points: 5 }),
         ]
       ),
-      choices("I love the culture of Nihon! Japan sugoi-amazing!", [
+      choices("I love the culture of Nihon! Japan is sugoi-amazing!", [
         choice({ label: "I totally agree.", points: 15 }),
         choice({ label: "What about your country?" }),
         choice({ label: "It's not that great.	" }),
       ]),
     ]),
-    LinkLevel(0, [
+    LinkLevel(5, [
       choices(
-        "You have gotten much better at this, Makoto-dono! Subarashii-wonderful!",
+        `You have gotten much better at this, ${mainCharName}-dono! Subarashii-wonderful!`,
         [
-          choice({ label: "I can do better." }),
+          choice({ label: "I can do better.", points: 5 }),
           choice({ label: "Thanks.", points: 5 }),
         ]
       ),
       choices("I would like to make something Japanese, but what?", [
         choice({ label: "What do you like?" }),
-        choice({ label: "Why not western clothes?" }),
+        choice({ label: "Why not western clothes?", points: 5 }),
         choice({ label: "How about a kimono?", points: 15 }),
       ]),
     ]),
-    LinkLevel(15, [
+    LinkLevel(20, [
       choices("......", [
         choice({ label: "How come you're not working?" }),
         choice({ label: "Should we stop for today?" }),
@@ -55,34 +56,34 @@ export const Temperance: SocialLinkType = {
       ]),
     ]),
     LinkLevel(15, [
-      choices("", [
+      choices("Could we go somwhere to eat after this?", [
         choice({ label: "Sure.", points: 5 }),
         choice({ label: "Why?" }),
       ]),
-      choices("", [
+      choices("I might never come back to Japan again!", [
         choice({ label: "You have to accept it." }),
         choice({ label: "Just stay in Japan!", points: 5 }),
       ]),
     ]),
-    LinkLevel(15, [
+    LinkLevel(20, [
       choices("I have barely sewn anything at all.", [
         choice({ label: "What's wrong?" }),
         choice({ label: "Why not take a break?", points: 5 }),
       ]),
       choices("Will you go to Azuki Arai with moi?", [
         choice({ label: "Let's do it.", points: 5 }),
-        choice({ label: "Just one minute." }),
+        choice({ label: "Just one minute...", points: 5 }),
       ]),
       choices("I want to stay here in Japan even if I have to eat dirt!", [
         choice({ label: "I have your back!", points: 15 }),
-        choice({ label: "What will you do?" }),
+        choice({ label: "What will you do?", points: 5 }),
       ]),
     ]),
     LinkLevel(15, [
       choices(
         "I will show him a kimono! When he sees it, he will understand the beauty of Nihon!",
         [
-          choice({ label: "Will that be enough?" }),
+          choice({ label: "Will that be enough?", points: 5 }),
           choice({ label: "That's a great idea.", points: 15 }),
         ]
       ),
@@ -91,7 +92,7 @@ export const Temperance: SocialLinkType = {
       choices(
         "When my uncle sees this, I know he'll agree with me about how great Nihon is!",
         [
-          choice({ label: "When will it be done?" }),
+          choice({ label: "When will it be done?", points: 5 }),
           choice({ label: "He'll definitely agree!", points: 15 }),
           choice({ label: "Less talk, more work." }),
         ]
@@ -99,7 +100,7 @@ export const Temperance: SocialLinkType = {
     ]),
     LinkLevel(22, [
       choices(
-        "And thanks to all your help, the kimono is almost finished! I feel so blessed.",
+        "And thanks to all your help, the kimono is almost finished! I feel so blessed!",
         [
           choice({ label: "Congrats!", points: 5 }),
           choice({ label: "Aren't you homesick?", points: 15 }),
@@ -110,7 +111,7 @@ export const Temperance: SocialLinkType = {
     LinkLevel(22, [
       choices("At last, it is fini!", [
         choice({ label: "How does it look?", points: 15 }),
-        choice({ label: "Great Work!", points: 15 }),
+        choice({ label: "Great work!", points: 15 }),
       ]),
       choices("They are the times I spend with you, my tomodachi.", [
         choice({ label: "I'll be waiting for you.", points: 15 }),
