33d46fb8f2641757b90b255f67f3eac16a51c0d9
dmitriy <dmitriyvasil@gmail.com>
2024-07-22 18:14:07 +0200

    add missing episodes

--- src/constants/events/index.ts
+++ src/constants/events/index.ts
@@ -1,9 +1,12 @@
+import { ShinjiroAragakiEpisodes } from "./ShinjiroAragakiEpisodes";
+import { RyojiMochizukiEpisodes } from "./RyojiMochizukiEpisodes";
 import { AkihikoSanadaEpisodes } from "./AkihikoSanadaEpisodes";
 import { statsEventsAcademics } from "./statsEventsAcademics";
 import { statsEventsCourage } from "./statsEventsCourage";
 import { JunpeiIoriEpisodes } from "./JunpeiIoriEpisodes";
 import { statsEventsCharm } from "./statsEventsCharm";
 import { KoromaruEpisodes } from "./KoromaruEpisodes";
+import { KenAmadaEpisodes } from "./KenAmadaEpisodes";
 import { specialEvents } from "./specialEvents";
 import { pcPrograms } from "./pcPrograms";
 import { linkEvents } from "./linkEvents";
@@ -18,4 +21,7 @@ export const events = {
   ...JunpeiIoriEpisodes,
   ...AkihikoSanadaEpisodes,
   ...KoromaruEpisodes,
+  ...KenAmadaEpisodes,
+  ...RyojiMochizukiEpisodes,
+  ...ShinjiroAragakiEpisodes,
 };
