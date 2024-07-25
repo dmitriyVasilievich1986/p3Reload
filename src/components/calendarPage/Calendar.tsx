51b5b30e6f5a18c3437e248bf320e1410a782fa3
dmitriy <dmitriyvasil@gmail.com>
2024-07-25 17:29:05 +0200

    add tooltip

--- src/components/calendarPage/Calendar.tsx
+++ src/components/calendarPage/Calendar.tsx
@@ -3,6 +3,7 @@ import classnames from "classnames/bind";
 import { CalendarProps } from "./types";
 import SocialLinks from "./SocialLinks";
 import * as style from "./style.scss";
+import { Tooltip } from "../tootlip";
 import HeroStats from "./HeroStats";
 import DayEvent from "./DayEvent";
 import moonIcon from "./moon.png";
@@ -18,7 +19,14 @@ function CurrentDate({ foolMoon, isDayOff, exams, date }: CalendarProps) {
   return (
     <div className={cx("date", { foolMoon, isDayOff, exams })}>
       <h1>{`${month} ${day} (${dayName})`}</h1>
-      {!!foolMoon && <img src={moonIcon} />}
+      <Tooltip
+        position="bottom"
+        tooltip={
+          <div style={{ width: "70px", textAlign: "center" }}>Full Moon</div>
+        }
+      >
+        {!!foolMoon && <img src={moonIcon} />}
+      </Tooltip>
     </div>
   );
 }
