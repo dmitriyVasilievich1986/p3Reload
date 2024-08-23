82fe9124f98f426af83f7b32772ed0ff14f9b607
dmitriy <dmitriyvasil@gmail.com>
2024-08-23 21:25:53 +0200

    update card need calculator

--- src/constants/socialLinks/calculationFunctions.ts
+++ src/constants/socialLinks/calculationFunctions.ts
@@ -47,7 +47,7 @@ export class CardNeededCalculator {
     const nextLevelRoundsWCard = CardNeededCalculator.nextLevelRounds(
       this.nextLevelPoints,
       maxPointsSumWCard,
-      this.multiplier
+      this.multiplier * this.cardMultiplier
     );
 
     return (
