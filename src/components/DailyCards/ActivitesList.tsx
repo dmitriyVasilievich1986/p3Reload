4e875965aa97588587ae9418d202a062fb8b34ac
dmitriy <dmitriyvasil@gmail.com>
2024-09-08 16:02:22 +0200

    fix imports

--- src/components/DailyCards/ActivitesList.tsx
+++ src/components/DailyCards/ActivitesList.tsx
@@ -1,15 +1,12 @@
-import { MonthNames, DaysNamesIndex } from "@/constants/monthsNames";
 import { SingleDay } from "@/constants/calendar/SingleDay";
 import { Times } from "@/constants/events/types";
 
-import { Tooltip } from "@/components/tootlip";
 import { Card } from "@/components/card";
 
 import { CurrentDate } from "./CurrentDate";
 import classnames from "classnames/bind";
 import { Activity } from "./Activity";
 import * as style from "./style.scss";
-import moonIcon from "./moon.png";
 
 const cx = classnames.bind(style);
 
