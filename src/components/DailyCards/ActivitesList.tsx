469662962a764b1b77aef746b3288dacb51fc037
dmitriy <dmitriyvasil@gmail.com>
2024-09-08 16:01:01 +0200

    move `CurrentDate` from `ActivitesList`

--- src/components/DailyCards/ActivitesList.tsx
+++ src/components/DailyCards/ActivitesList.tsx
@@ -5,32 +5,13 @@ import { Times } from "@/constants/events/types";
 import { Tooltip } from "@/components/tootlip";
 import { Card } from "@/components/card";
 
+import { CurrentDate } from "./CurrentDate";
 import classnames from "classnames/bind";
 import { Activity } from "./Activity";
 import * as style from "./style.scss";
 import moonIcon from "./moon.png";
 
 const cx = classnames.bind(style);
-
-export function CurrentDate({ currentDay }: { currentDay: SingleDay }) {
-  const dayName = DaysNamesIndex[currentDay.date.getDay()];
-  const month = MonthNames[currentDay.date.getMonth()];
-  const day = currentDay.date.getDate();
-
-  return (
-    <div className={cx("date", { ...currentDay })}>
-      <h1>{`${month} ${day} (${dayName})`}</h1>
-      <Tooltip
-        position="bottom"
-        tooltip={
-          <div style={{ width: "70px", textAlign: "center" }}>Full Moon</div>
-        }
-      >
-        {!!currentDay.foolMoon && <img src={moonIcon} />}
-      </Tooltip>
-    </div>
-  );
-}
 
 export function ActivitesList(props: {
   clickHandler: (time: Times) => void;
--- src/components/DailyCards/index.ts
+++ src/components/DailyCards/index.ts
@@ -1,6 +1,7 @@
 import { SocialLinksStats } from "./SocialLinksStats";
 import { ActivitesList } from "./ActivitesList";
+import { CurrentDate } from "./CurrentDate";
 import { HeroStats } from "./HeroStats";
 import { Activity } from "./Activity";
 
-export { SocialLinksStats, ActivitesList, HeroStats, Activity };
+export { SocialLinksStats, ActivitesList, CurrentDate, HeroStats, Activity };
