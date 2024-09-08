a70705cd718416491ff64637cad6f0b766b9f4c2
dmitriy <dmitriyvasil@gmail.com>
2024-09-08 14:39:53 +0200

    add `highlited` props

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,10 +1,9 @@
-import { CardProps, HeadProps } from "./types";
 import classnames from "classnames/bind";
 import * as style from "./style.scss";
 
 const cx = classnames.bind(style);
 
-function Head(props: HeadProps) {
+function Head(props: { head?: string }) {
   if (!props?.head) return null;
   return <legend className={cx("head")}>{props.head}</legend>;
 }
@@ -14,12 +13,21 @@ function Badge(props: { children?: React.ReactNode }) {
   return <div className="card-badge">{props.children}</div>;
 }
 
-function Card(props: CardProps) {
+function Card(props: {
+  onClick?: () => void;
+  color?: "primary" | "secondary";
+  children?: React.ReactNode;
+  badge?: React.ReactNode;
+  highlited?: boolean;
+  enable?: boolean;
+  head?: string;
+}) {
   return (
     <fieldset
       onClick={props?.onClick}
       className={cx("card", {
         enable: props?.enable,
+        highlited: props?.highlited,
         primary: props?.color === "primary",
       })}
     >
