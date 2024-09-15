f9a7a0940ed542aa7fe93fe83184e0006ebd1d66
dmitriy <dmitriyvasil@gmail.com>
2024-09-15 09:46:53 +0200

    add events filter in left bar

--- src/components/leftBar/LeftBar.tsx
+++ src/components/leftBar/LeftBar.tsx
@@ -1,22 +1,48 @@
 import { dayIndexParams } from "@/components/supportComponents";
-import { calendar } from "@/constants/calendar";
+import { SingleDay } from "@/constants/calendar";
 
 import classnames from "classnames/bind";
 import * as style from "./style.scss";
 import MonthBlock from "./MonthBlock";
+import { Input } from "../input";
+import React from "react";
 
 const cx = classnames.bind(style);
 
-function LeftBar() {
-  const allMonths = new Set(calendar.map((c) => c.date.getMonth()));
+function LeftBar(props: { calendar: SingleDay[] }) {
+  const [filter, setFilter] = React.useState("");
   const [_, updateDayIndex] = dayIndexParams();
+  const [filteredCalendar, setFilteredCalendar] = React.useState<SingleDay[]>(
+    []
+  );
+
+  React.useEffect(() => {
+    if (filter === "") {
+      setFilteredCalendar(props.calendar);
+    } else {
+      const newCalendar = props.calendar.filter((c) => {
+        const names = c.activities.map((a) =>
+          (a.name.toLowerCase() as String).includes(filter.toLowerCase())
+        );
+        return names.includes(true);
+      });
+      setFilteredCalendar(newCalendar);
+    }
+  }, [props.calendar, filter]);
+
+  const allMonths = new Set(filteredCalendar.map((c) => c.date.getMonth()));
 
   return (
     <div className={cx("left-bar")}>
+      <div className={cx("events-filter")}>
+        <div>
+          <Input onChange={setFilter} value={filter} />
+        </div>
+      </div>
       <div>
         {Array.from(allMonths).map((m) => (
           <MonthBlock
-            days={calendar.filter((c) => c.date.getMonth() === m)}
+            days={filteredCalendar.filter((c) => c.date.getMonth() === m)}
             clickHandler={updateDayIndex}
             key={m}
           />
