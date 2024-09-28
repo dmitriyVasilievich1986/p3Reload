baa754987f096e6f3f6fb7505fc6a1ac3810ed6a
dmitriy <dmitriyvasil@gmail.com>
2024-09-28 12:12:29 +0200

    fix `isAvailable` function

--- src/constants/socialLinks/Mochizuki.tsx
+++ src/constants/socialLinks/Mochizuki.tsx
@@ -25,16 +25,16 @@ class MochizukiMainLevels extends LinkMainLevelsEpisodes {
           new Date(2009, 10, 14).getTime(),
           new Date(2009, 10, 16).getTime(),
         ];
-        return days.includes(props.currentDay.date.getDay()) && isTime;
+        return days.includes(props.currentDay.date.getTime()) && isTime;
       case 2:
         days = [new Date(2009, 10, 18).getTime()];
-        return days.includes(props.currentDay.date.getDay()) && isTime;
+        return days.includes(props.currentDay.date.getTime()) && isTime;
       case 3:
         days = [new Date(2009, 11, 1).getTime()];
-        return days.includes(props.currentDay.date.getDay()) && isTime;
+        return days.includes(props.currentDay.date.getTime()) && isTime;
       case 4:
         days = [new Date(2009, 11, 31).getTime()];
-        return days.includes(props.currentDay.date.getDay()) && isTime;
+        return days.includes(props.currentDay.date.getTime()) && isTime;
       default:
         return false;
     }
