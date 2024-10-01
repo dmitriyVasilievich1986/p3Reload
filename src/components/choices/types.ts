872e7bddca6a4f74626519b2dbdf3301252de532
dmitriy <dmitriyvasil@gmail.com>
2024-10-01 16:57:51 +0200

    replace main char name inside Q/A components

--- src/components/choices/types.ts
+++ src/components/choices/types.ts
@@ -1,11 +1,5 @@
 import React from "react";
 
-export type ChoiceProps = {
-  label: string;
-  points?: number;
-  fork?: boolean;
-};
-
 export type ChoicesProps = {
   label: string;
   children?: React.ReactNode;
