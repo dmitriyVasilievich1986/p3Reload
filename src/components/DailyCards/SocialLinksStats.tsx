4ea9966663bd76397c03461fbaf315889ea63357
dmitriy <dmitriyvasil@gmail.com>
2024-10-06 22:45:54 +0200

    rewrite `SocialLinksStats` component

--- src/components/DailyCards/SocialLinksStats.tsx
+++ src/components/DailyCards/SocialLinksStats.tsx
@@ -1,73 +1,34 @@
-import { SocialLinkNames, socialLinks } from "@/constants/socialLinks";
+import { SocialLinkNames } from "@/constants/socialLinks/types";
 import { SingleDay } from "@/constants/calendar/SingleDay";
+import { LinksStats } from "./LinksStats";
 
-import { Tooltip } from "@/components/tootlip";
-import { Badge } from "@/components/badge";
-import { Card } from "@/components/card";
-
-import { BadgeTooltip } from "./BadgeTooltip";
-import classnames from "classnames/bind";
-import * as style from "./style.scss";
-
-const cx = classnames.bind(style);
-
-export function SocialLinksStats({
-  previousDay,
-  currentDay,
-}: {
+export function SocialLinksStats(props: {
   previousDay: SingleDay;
   currentDay: SingleDay;
 }) {
-  const getLevel = (name: SocialLinkNames) => {
-    if (currentDay.links[name].level === 0) return "- not established";
-    if (currentDay.links[name].level >= socialLinks[name].maxLevel)
-      return "- max level";
-    return null;
-  };
+  const mainLinks: SocialLinkNames[] = [
+    SocialLinkNames.Fool,
+    SocialLinkNames.Magician,
+    SocialLinkNames.Priestess,
+    SocialLinkNames.Empress,
+    SocialLinkNames.Emperor,
+    SocialLinkNames.Hierophant,
+    SocialLinkNames.Lovers,
+    SocialLinkNames.Chariot,
+    SocialLinkNames.Justice,
+    SocialLinkNames.Hermit,
+    SocialLinkNames.Fortune,
+    SocialLinkNames.Strength,
+    SocialLinkNames.HangedMan,
+    SocialLinkNames.Death,
+    SocialLinkNames.Temperance,
+    SocialLinkNames.Devil,
+    SocialLinkNames.Tower,
+    SocialLinkNames.Star,
+    SocialLinkNames.Moon,
+    SocialLinkNames.Sun,
+    SocialLinkNames.Aeon,
+  ];
 
-  const isLeveledUp = (name: SocialLinkNames) => {
-    if (previousDay === undefined) return false;
-    return previousDay.links[name].level !== currentDay.links[name].level;
-  };
-
-  const getBadge = (name: SocialLinkNames) => {
-    const color = isLeveledUp(name) ? "green" : "blue";
-    return (
-      <Badge
-        value={String(currentDay.links[name].level)}
-        color={color}
-        size="small"
-      />
-    );
-  };
-
-  const StatsTooltip = ({ name }: { name: SocialLinkNames }) => {
-    const nextLevelPoints = socialLinks[name].getLevel({
-      ...currentDay.links[name],
-    }).points;
-    return (
-      <BadgeTooltip
-        points={currentDay.links[name].points}
-        nextLevel={nextLevelPoints}
-      />
-    );
-  };
-
-  return (
-    <Card head="Links" color="primary">
-      <div className={cx("stat")}>
-        {(Object.keys(currentDay.links) as Array<SocialLinkNames>)
-          .sort((l) => (isLeveledUp(l) ? -1 : 1))
-          .map((l) => (
-            <div key={l} className={cx("stat-item")}>
-              <Tooltip position="right" tooltip={<StatsTooltip name={l} />}>
-                {getBadge(l)}
-              </Tooltip>
-              <label>{l}</label>
-              {getLevel(l)}
-            </div>
-          ))}
-      </div>
-    </Card>
-  );
+  return LinksStats({ ...props, linksNames: mainLinks, head: "Links" });
 }
