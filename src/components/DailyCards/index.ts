61b0dc252f6cf4d2129bb073aeac9b654fd83b4f
dmitriy <dmitriyvasil@gmail.com>
2024-10-06 22:46:42 +0200

    add new stats components

--- src/components/DailyCards/index.ts
+++ src/components/DailyCards/index.ts
@@ -1,7 +1,17 @@
 import { SocialLinksStats } from "./SocialLinksStats";
+import { DormHangoutStats } from "./DormHangoutStats";
 import { ActivitesList } from "./ActivitesList";
+import { EpisodesStats } from "./EpisodesStats";
 import { CurrentDate } from "./CurrentDate";
 import { HeroStats } from "./HeroStats";
 import { Activity } from "./Activity";
 
-export { SocialLinksStats, ActivitesList, CurrentDate, HeroStats, Activity };
+export {
+  DormHangoutStats,
+  SocialLinksStats,
+  ActivitesList,
+  EpisodesStats,
+  CurrentDate,
+  HeroStats,
+  Activity,
+};
