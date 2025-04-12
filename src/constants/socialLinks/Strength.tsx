1f00aaa75d392019f2f98268f7077414ed3d83bb
dmitriy <dmitriyvasil@gmail.com>
2025-04-12 13:09:36 +0200

    feat: update `isAvailable` for the `Strength` link

--- src/constants/socialLinks/Strength.tsx
+++ src/constants/socialLinks/Strength.tsx
@@ -1,44 +1,40 @@
 import { QuestionsWrapper, Question, Answer } from "@/components";
 
+import availables from "@/constants/availability/AvailableClass";
 import { DaysNames } from "@/constants/monthsNames";
 import { Times } from "@/constants/events/types";
 
-import { createBondObject, LinkMaxedObject } from "./classes/GenericCard";
-import { SocialLink } from "./classes/SocialLink";
-
+import { SocialLink } from "@/constants/socialLinks/classes/SocialLink";
+import {
+  createBondObject,
+  LinkMaxedObject,
+} from "@/constants/socialLinks/classes/GenericCard.tsx";
 import {
   KoromaruWalkSocialLinkLevels,
   InvitationLevels,
   LinkMainLevels,
-} from "./classes/LinkLevels";
-
+} from "@/constants/socialLinks/classes/LinkLevels";
 import {
-  EventAvailableProps,
   SocialLinkNames,
   LevelsType,
   Routes,
-} from "./types";
+} from "@/constants/socialLinks/types";
 
 class StrengthMainLevels extends LinkMainLevels {
-  isAvailable(props: EventAvailableProps): boolean {
-    const linkName = props.socialLink.linkName;
-    const previousLink = props.previousDay!.links[linkName];
-    const isNewLevel = props.socialLink.isNewLevel(previousLink);
-    const isRomance =
-      previousLink.level === 2 || previousLink.romance === props.route;
-    const days = [DaysNames.wednesday, DaysNames.saturday];
-
-    return (
-      props.currentDay.date.getTime() >= new Date(2009, 3, 24).getTime() &&
-      props.previousDay!.links[SocialLinkNames.Chariot].level >= 2 &&
-      days.includes(props.currentDay.date.getDay()) &&
-      !props.currentDay.isDayOff &&
-      props.time === Times.Day &&
-      !props.currentDay.exams &&
-      isNewLevel &&
-      isRomance
-    );
-  }
+  isAvailable = new availables.And_([
+    new availables.AvailableIsDayOff({ reverse: true, isExamIncluded: true }),
+    new availables.AvailableDateGreater({ date: new Date(2009, 3, 24) }),
+    new availables.AvailableTimesIsIn({ times: [Times.Day] }),
+    new availables.AvailableLinkRoute({ forkLevel: 2 }),
+    new availables.AvailableLinkIsNewLevel(),
+    new availables.AvailableLinkLevelGreater({
+      name: SocialLinkNames.Chariot,
+      level: 2,
+    }),
+    new availables.AvailableDaysNamesIsIn({
+      days: [DaysNames.wednesday, DaysNames.saturday],
+    }),
+  ]).available;
 
   levels: LevelsType = {
     0: {
