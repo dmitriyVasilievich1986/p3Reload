cff2b7cae498edaf61c7d6a70cc9da834767f5c9
dmitriy <dmitriyvasil@gmail.com>
2025-04-13 11:17:26 +0200

    feat: add `Prerequisits` category

--- src/constants/events/types.ts
+++ src/constants/events/types.ts
@@ -11,6 +11,7 @@ import {
 } from "@/constants/socialLinks/types";
 
 export enum Categories {
+  Prerequisits = "Prerequisits",
   Invitation = "Invitation",
   Tartarus = "Tartarus",
   Special = "Special",
