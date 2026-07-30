4191ac2180d99da7a6c61b83eb8a23914aeb425a
dmitriy <dmitriyvasil@gmail.com>
2026-07-30 16:32:50 +0200

    feat: add date and time constants
    - Introduced new constants for date formatting and day of the week.
    - Added `DatesFormat` for consistent date representation.
    - Created `DayOfWeek` and `DayOfWeekPosition` for managing days and their respective positions.
    - Included `Times` constants for various time periods in the game.

---
+++ src/constants/dates.ts
@@ -0,0 +1,1 @@
+export const DatesFormat = 'YYYY-MM-DD';
---
+++ src/constants/dayOfWeek.ts
@@ -0,0 +1,22 @@
+export const DayOfWeek: Record<string, string> = {
+  Monday: 'Monday',
+  Tuesday: 'Tuesday',
+  Wednesday: 'Wednesday',
+  Thursday: 'Thursday',
+  Friday: 'Friday',
+  Saturday: 'Saturday',
+  Sunday: 'Sunday',
+};
+
+export type DayOfWeekType = (typeof DayOfWeek)[keyof typeof DayOfWeek];
+
+// for dayjs
+export const DayOfWeekPosition: Record<DayOfWeekType, number> = {
+  [DayOfWeek.Monday]: 1,
+  [DayOfWeek.Tuesday]: 2,
+  [DayOfWeek.Wednesday]: 3,
+  [DayOfWeek.Thursday]: 4,
+  [DayOfWeek.Friday]: 5,
+  [DayOfWeek.Saturday]: 6,
+  [DayOfWeek.Sunday]: 7,
+};
