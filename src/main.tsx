2dcd27e1127c231d97905bfe9895fc92489eabd2
dmitriy <dmitriyvasil@gmail.com>
2026-07-30 10:22:51 +0200

    feat: Temporary disable project

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,11 @@
-import { BrowserRouter } from "react-router-dom";
-import ReactDOM from "react-dom/client";
-import React from "react";
-import App from "./App";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/p3Reload">
-      <App />
-    </BrowserRouter>
+    <BrowserRouter basename="/p3Reload"></BrowserRouter>
   </React.StrictMode>
 );
