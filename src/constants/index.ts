26ffb184b0f69f640aa87ad3cf689e546b381d19
dmitriy <dmitriyvasil@gmail.com>
2026-07-31 15:19:15 +0200

    feat: export Arcanas constants and type from arcanas module
    - Added export for Arcanas and its type definition from the arcanas module to enhance accessibility and maintain consistency in the codebase.

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -5,3 +5,4 @@
 export { DayOfWeek, type DayOfWeekType, DayOfWeekPosition } from './dayOfWeek';
 export { Times, type TimesType } from './times';
 export { DatesFormat, FullMoonDays, ExamDays, DayOffDays } from './dates';
+export { Arcanas, type ArcanasType } from './arcanas';
