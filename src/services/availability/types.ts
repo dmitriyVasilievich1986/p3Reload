f26392863f81cb9ee9c9d545da5931a6b54cec8f
dmitriy <dmitriyvasil@gmail.com>
2026-08-05 10:46:09 +0200

    refactor: update import statement for SocialLinkStats to use type import
    - Changed the import of SocialLinkStats to a type import for improved type safety and clarity in the availability model props.

--- src/services/availability/types.ts
+++ src/services/availability/types.ts
@@ -1,11 +1,10 @@
-/**
- * Type definitions and constants for availability model props.
- */
-import { SocialLinkStats } from '@services/socialLinkStats';
-
 import type { TimesType } from '@constants/times';
 import type { AdditionalStats } from '@services/additionalStats';
 import type { CharacterStats } from '@services/characterStats';
+/**
+ * Type definitions and constants for availability model props.
+ */
+import type { SocialLinkStats } from '@services/socialLinkStats';
 
 export type IsAvailableProps = {
   time: TimesType;
