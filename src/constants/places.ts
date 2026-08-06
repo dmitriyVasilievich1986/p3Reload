a9ca09007156adba145a643e0aa67b671d3b621e
dmitriy <dmitriyvasil@gmail.com>
2026-08-06 10:10:58 +0200

    feat: add Places and Districts constants for location management
    - Introduced new constants for Places and Districts to represent various locations within the application.
    - Updated the index file to export these new constants, enhancing module accessibility and organization.

---
+++ src/constants/places.ts
@@ -0,0 +1,17 @@
+export const Places = {
+  NurseOffice: 'Nurse Office',
+  Dorm: 'Dorm',
+  GameParade: 'Game Parade',
+  WakatsuKitchen: 'Wakatsu Kitchen',
+  ChagallCafe: 'Chagall Cafe',
+  HagakureRamen: 'Hagakure Ramen',
+  Tartarus: 'Tartarus',
+  WilduckBurger: 'Wilduck Burger',
+} as const;
+
+export const Districts = {
+  IwatodaiDormitory: 'Iwatodai Dormitory',
+  PaulowniaMall: 'Paulownia Mall',
+  GekkoukanHighSchool: 'Gekkoukan High School',
+  IwatodaiStripMall: 'Iwatodai Strip Mall',
+} as const;
