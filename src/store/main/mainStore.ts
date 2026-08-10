eaa7e1d9b26832bc664175df480bcada467e4fd9
dmitriy <dmitriyvasil@gmail.com>
2026-08-10 10:40:46 +0200

    fix: ensure newline at end of file in main store index and refactor selectedTimes setter for clarity
    - Added a newline at the end of the index.ts file to comply with coding standards.
    - Refactored the setter for selectedTimes in mainStore.ts to improve readability by formatting the function definition across multiple lines.

--- src/store/main/mainStore.ts
+++ src/store/main/mainStore.ts
@@ -1,10 +1,10 @@
 import { create } from 'zustand';
 import { devtools } from 'zustand/middleware';
+
+import type { MainStoreInitialState } from './types';
 import type { TimesType } from '@constants/times';
 import type { Calendar } from '@services/calendar';
 import type { Day } from '@services/day';
-
-import type { MainStoreInitialState } from './types';
 
 export const useMainStore = create<MainStoreInitialState>()(
   devtools((set) => ({
@@ -15,6 +15,7 @@ export const useMainStore = create<MainStoreInitialState>()(
     setIsLoading: (isLoading: boolean) => set({ isLoading }, undefined, 'setIsLoading'),
     setCalendar: (calendar: Calendar | null) => set({ calendar }, undefined, 'setCalendar'),
     setCurrentDay: (currentDay: Day | null) => set({ currentDay }, undefined, 'setCurrentDay'),
-    setSelectedTimes: (time: TimesType | null) => set({ selectedTimes: time }, undefined, 'setSelectedTimes'),
+    setSelectedTimes: (time: TimesType | null) =>
+      set({ selectedTimes: time }, undefined, 'setSelectedTimes'),
   }))
 );
