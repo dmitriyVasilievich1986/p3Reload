b9c5d021edde13919889d9e25560242a5178d248
dmitriyVasilievich1986 <54740280+dmitriyVasilievich1986@users.noreply.github.com>
2026-08-10 10:48:47 +0200

    Feature :: Add store (#119)
    * chore: update dependencies in package.json and package-lock.json
    
    - Added zustand version 5.0.14 to both package.json and package-lock.json for state management.
    - Updated nanoid version from 3.3.16 to 3.3.18 in package-lock.json for improved unique ID generation.
    - Changed dev property to devOptional for certain dependencies in package-lock.json to enhance clarity in dependency management.
    
    * feat: add alias for store directory in Vite configuration
    
    - Introduced a new alias '@store' in the Vite configuration to simplify imports from the store directory, enhancing code organization and readability.
    
    * feat: implement main store with Zustand for state management
    
    - Introduced the main store using Zustand, encapsulating state management for loading status, calendar, current day, and selected times.
    - Added initial state and setter functions for updating store values.
    - Created unit tests to validate the functionality of the main store, ensuring expected behavior for state initialization and updates.
    
    * fix: ensure newline at end of file in main store index and refactor selectedTimes setter for clarity
    
    - Added a newline at the end of the index.ts file to comply with coding standards.
    - Refactored the setter for selectedTimes in mainStore.ts to improve readability by formatting the function definition across multiple lines.
    
    * refactor: rename MainStoreInitialState to MainStoreType for clarity
    
    - Updated the type definition for the main store from MainStoreInitialState to MainStoreType to better reflect its purpose and improve code readability.
    - Adjusted the usage in mainStore.ts to align with the new type definition.

---
+++ src/store/main/types.ts
@@ -0,0 +1,14 @@
+import type { TimesType } from '@constants/times';
+import type { Calendar } from '@services/calendar';
+import type { Day } from '@services/day';
+
+export type MainStoreType = {
+  isLoading: boolean;
+  calendar: Calendar | null;
+  currentDay: Day | null;
+  selectedTimes: TimesType | null;
+  setIsLoading: (isLoading: boolean) => void;
+  setCalendar: (calendar: Calendar | null) => void;
+  setCurrentDay: (currentDay: Day | null) => void;
+  setSelectedTimes: (time: TimesType | null) => void;
+};
