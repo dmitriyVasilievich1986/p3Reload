5d0233292960f1ca9824cdbfb7b7334fd1ead095
dmitriy <dmitriyvasil@gmail.com>
2026-08-10 11:14:44 +0200

    feat: add Tailwind CSS styles and integrate into main application
    - Created a new index.css file to include Tailwind CSS base, components, and utilities for enhanced styling.
    - Imported the new CSS file in main.tsx to apply styles across the application.

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router';
 
+import './index.css';
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter basename="/p3Reload"></BrowserRouter>
