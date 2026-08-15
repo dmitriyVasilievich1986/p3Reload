5db584b7a97791bff08efb2de01e64e3a2c6da23
dmitriy <dmitriyvasil@gmail.com>
2026-08-15 08:54:05 +0200

    feat: add EmptyEvent class for handling blank events
    - Introduced the EmptyEvent class extending BaseEvent to represent blank events in the system.
    - Implemented a static render method for rendering a placeholder card with a "Do nothing" message.
    - Updated the instance render method to utilize the static render method, enhancing code reusability.

--- src/services/event/models/specialEvents/EmptyEvent.tsx
+++ src/services/event/models/specialEvents/EmptyEvent.tsx
@@ -1,3 +1,5 @@
+import { Card } from '@components/card';
+import { TextRow } from '@components/row';
 import { BaseEvent } from '@services/event/base';
 
 import { SpecialEventsNames, type SpecialEventsNamesType } from './types';
@@ -8,8 +10,17 @@ import type { Stats } from '@services/stats';
 export class EmptyEvent extends BaseEvent {
   static readonly name: SpecialEventsNamesType = SpecialEventsNames.Empty;
 
-  render(this: EmptyEvent, _props: IsAvailableProps): React.ReactNode {
-    return null;
+  static render(this: typeof EmptyEvent, props: IsAvailableProps): React.ReactNode {
+    return (
+      <Card
+        header="Blank event"
+        body={<TextRow key={`empty-event-${props.time}`} text="Do nothing" />}
+      />
+    );
+  }
+
+  render(this: EmptyEvent, props: IsAvailableProps): React.ReactNode {
+    return (this.constructor as typeof EmptyEvent).render(props);
   }
 
   calculateStats(this: EmptyEvent, _props: IsAvailableProps): Stats {
