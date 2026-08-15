7350e10e7d390aa4ede2e1a4af3d98ccbb2807dd
dmitriy <dmitriyvasil@gmail.com>
2026-08-15 08:50:26 +0200

    feat: implement static render method for CharacterStatsModifyEventBase
    - Added a static render method to the CharacterStatsModifyEventBase class for improved rendering of character stats modification events.
    - Integrated Card and LabelRow components for structured display of event details, including price, place, district, and modifiers.
    - Updated the instance render method to utilize the new static render method, enhancing code clarity and reusability.

--- src/services/event/models/characterStatsModifyEvents/base.tsx
+++ src/services/event/models/characterStatsModifyEvents/base.tsx
@@ -1,4 +1,7 @@
+import { Card } from '@components/card';
+import { LabelRow } from '@components/row';
 import { BaseEvent } from '@services/event/base';
+import { priceFormatter } from '@utils/priceFormatter';
 
 import type { CharacterStatsModifyNamesType } from './types';
 import type { IsAvailableProps } from '@services/availability/types';
@@ -21,7 +24,36 @@ export abstract class CharacterStatsModifyEventBase extends BaseEvent {
     return this.stats.updateCharacterStats(characterStats);
   }
 
-  render(_props: IsAvailableProps): React.ReactNode {
-    return null;
+  static render(
+    this: typeof CharacterStatsModifyEventBase,
+    props: IsAvailableProps
+  ): React.ReactNode {
+    return (
+      <Card
+        key={`${this.name}-${props.time}`}
+        body={
+          <>
+            {this.price && (
+              <LabelRow key="price" label="Price:" text={priceFormatter(this.price)} />
+            )}
+            <LabelRow key="place" label="Place:" text={this.place} />
+            <LabelRow key="district" label="District:" text={this.district} />
+            <LabelRow key="stats" label="Stats:" text={''} />
+            <ul className="list-disc pl-5 text-sm text-slate-800 dark:text-slate-100">
+              {this.modifiers.map((modifier) => (
+                <li key={modifier.name}>
+                  {`${modifier.name} ${modifier.operator}${modifier.value}`}
+                </li>
+              ))}
+            </ul>
+          </>
+        }
+        header={this.header}
+      />
+    );
+  }
+
+  render(props: IsAvailableProps): React.ReactNode {
+    return (this.constructor as typeof CharacterStatsModifyEventBase).render(props);
   }
 }
